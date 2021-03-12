5f8708fa0441bc272077993a11667013b81631c1
Ben Jardine <jardine_199@hotmail.co.uk>
2021-03-12 15:01:51 +0000

    updated the states for ToDos

--- src/components/InputToDo.js
+++
@@ -1,1 +0,0 @@
-InputToDo.js
--- src/index.js
+++ src/index.js
@@ -5,3 +5,5 @@ import ReactDOM from "react-dom"
 import ToDoContainer from "./components/ToDoContainer"
 
 ReactDOM.render(<ToDoContainer/>, document.getElementById("root"))
+
+
