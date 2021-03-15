6c3fe847bbe7d5cb8917a0d32a5f4c4192565173
Ben Jardine <jardine_199@hotmail.co.uk>
2021-03-15 19:04:09 +0000

    adding checkboxes

--- src/components/ToDoContainer.js
+++ src/components/ToDoContainer.js
@@ -1,6 +1,7 @@
 import React from "react"
 
-import TodosList from "./TodosList";
+import TodosList from "./TodosList"
+import Header from "./Header"
 
 class TodoContainer extends React.Component {
 
@@ -27,6 +28,7 @@ class TodoContainer extends React.Component {
       render() {
         return (
           <div>
+            <Header />
             <TodosList todos={this.state.todos} />
           </div>
         );
