055110fece4bf74cce02bd7d0e604c922f9c955d
Ben Jardine <jardine_199@hotmail.co.uk>
2021-03-29 16:25:05 +0100

    fetch method for 200 todos list

--- src/components/ToDoContainer.js
+++ src/components/ToDoContainer.js
@@ -57,6 +57,12 @@ class TodoContainer extends React.Component {
       })
     }
 
+    componentDidMount() {
+      fetch("https://jsonplaceholder.typicode.com/todos")
+        .then(response => response.json())
+        .then(data => console.log(data));
+    }
+
 
       render() {
         return (
